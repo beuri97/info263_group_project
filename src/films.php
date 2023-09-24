c479417da62dd3cc075debc58fd1eb348dc3205a
Joel Dumelow <jdu89@uclive.ac.nz>
2023-09-24 15:44:07 +1300

    Added Film Descriptions to the films page while also fixing up some simple UI format.

--- src/films.php
+++ src/films.php
@@ -27,32 +27,47 @@
 
 <?php
 
+try {
+    $open_review_s_db = new PDO("sqlite:resources/star_wars.db");
+    $open_review_s_db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+} catch (PDOException $e) {
+    die($e->getMessage());
+}
 
 if (!isset($_GET["id"])) {
-    try {
-        $open_review_s_db = new PDO("sqlite:resources/star_wars.db");
-        $open_review_s_db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-    } catch (PDOException $e) {
-        die($e->getMessage());
-    }
 
     try {
         $res = $open_review_s_db->query("SELECT filmID, film_title, film_release_date, image_url FROM film");
         while($row = $res->fetch(PDO::FETCH_ASSOC)) {
-            echo $row['film_title'] . "<br />";
+            echo "<h2>" . $row['film_title'] . "</h2>";
             echo "<a href='films.php?id=" . $row['filmID'] ."'><img height='300' src='" . $row['image_url'] . "' /></a><br />";
-            echo $row['film_release_date'] . "<br /><br />";
         }
     } catch (PDOException $e) {
         die($e->getMessage());
     }
 } else {
-
+    try {
+        $res = $open_review_s_db->query("SELECT filmID, film_title, film_release_date, film_opening_crawl, image_url FROM film WHERE filmID = " . $_GET["id"]);
+        while($row = $res->fetch(PDO::FETCH_ASSOC)) {
+            echo "<h2>" . $row['film_title'] . "</h2>";
+            echo "<a href='films.php?id=" . $row['filmID'] ."'><img height='300' src='" . $row['image_url'] . "' /></a><br />";
+            echo "<b>Release Date: </b>" . $row['film_release_date'];
+            echo "<h2>Film Description:</h2>";
+            echo $row['film_opening_crawl'];
+        }
+    } catch (PDOException $e) {
+        die($e->getMessage());
+    }
 
 
 }
 
 ?>
+
+
+<ul>
+    <li><a href="films.php">Back</a></li>
+</ul>
 
 </body>
 </html>
