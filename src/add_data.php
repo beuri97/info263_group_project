54de4797e49ee1281c915d521ecfd65ae8cb280d
beuri97 <beuri97@gmail.com>
2023-10-11 02:34:12 +1300

    fix add_data.php

--- src/add_data.php
+++ src/add_data.php
@@ -59,17 +59,17 @@
         <table style="text-align: center">
             <tr>
                 <td><label for="input-title">Title</label></td>
-                <td id="td-title"><input id = "input-title" type="text" name = "title" ></td>
+                <td id="td-title"><input id = "input-title" type="text" name = "title" required></td>
                 <td><label for="input-episode">Episode</label></td>
-                <td colspan="3" id="td-episode"><input type="text" id="input-episode" name="episode" ></td>
+                <td colspan="3" id="td-episode"><input type="text" id="input-episode" name="episode" required></td>
                 <td><label for="input-director">Director</label></td>
-                <td id="td-director"><input type="text" id="input-director" name="director" ></td>
+                <td id="td-director"><input type="text" id="input-director" name="director" required></td>
                 <td><label for="input-date">Date</label></td>
-                <td id="td-date"><input type="date" id="input-date" name="release_date" ></td>
+                <td id="td-date"><input type="date" id="input-date" name="release_date" required></td>
             </tr>
             <tr>
                 <td><label for="input-crawl">Opening Crawl</label></td>
-                <td colspan="9" id="td-crawl"><textarea id="input-crawl" style="resize: none" rows="10" name="crawl">Enter text here.</textarea> </td>
+                <td colspan="9" id="td-crawl"><textarea id="input-crawl" style="resize: none" rows="10" name="crawl" required>Enter text here.</textarea> </td>
             </tr>
             <tr>
                 <td><label for="input-url">image URL</label></td>
@@ -80,66 +80,30 @@
         <input type="submit" id="submit" name="add" value="Add" style="margin-left: 1%" onclick="change_border_box()">
     </form>
 </div>
+<div id="add_people">
 
-<script>
-    function show_required (name) {
-        let INPUT_PREFIX = "input";
-        var field = document.getElementById(INPUT_PREFIX.concat("-", name)).value;
-        let TD_PREFIX = "td"
-        document.getElementById(TD_PREFIX.concat("-", name)).style.borderColor = (field.value == "" || field.value == null) ? "red" : "black";
-    }
+</div>
 
-    function change_border_box() {
-        let names = ["title", "episode", "director", "date", "crawl"];
-        for(let i=0; i<names.length; i++) {
-            show_required(names[i]);
-        }
-    }
-</script>
+
 <?php
+    include_once 'database.php';
 
-try {
-    $db = new PDO("sqlite:resources/star_wars.db");
-    $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-} catch (PDOException $e) {
-    die($e->getMessage());
-}
-// important
-if (isset($_POST['title']))
-{
+    // Initialise films attributes' values
     $title = $_POST['title'];
-} else {
-    $title = NULL;
-}
+    $episode = (int)$_POST['episode'];
+    $director = $_POST['director'];
+    $release = date("Y-m-d", strtotime($_POST['release_date']));
+    $crawl = $_POST['crawl'];
 
-$episode = $_POST['episode'];
-$director = $_POST['director'];
-$release = $_POST['release_date'];
-$crawl = $_POST['crawl'];
+    if(isset($_POST['add'], $title, $episode, $director, $release, $crawl) and $title != '' and $episode != '' and $director != ''
+        and $release != '' and $crawl != '') {
+        $url = ($_POST['url'] == '') ? NULL : $_POST['url'];
+        addFilm($title, $episode, $crawl, $director, $release, $url);
+    }
 
-echo $release;
 
-if(isset($_POST['add']) and $title != '' and $episode != '' and $director != ''
-    and $release != '' and $crawl != '') {
-    echo "HELLO IT starts here";
-    $result = $db->query("SELECT max(filmID) as num from film");
-    $resultNum = $result->fetch(PDO::FETCH_ASSOC);
-    $number = $resultNum['num'] + 1;
-    $url = ($_POST['url'] == '') ? NULL : $_POST['url'];
-    echo "test1";
-    $query = "INSERT INTO film(filmID, film_title, film_episode_id, film_opening_crawl, film_director, film_release_date, image_url)
-                values($number, $title, $episode, $crawl, $director, $release, $url)";
-    echo "test2";
-    $stmt = $db->prepare($query);
-    echo "test3";
-    $stmt->execute();
-    $db = null;
-    echo "SUCCESS";
 
-} else if(isset($_POST['add']) and ($title == '' or $episode == '' or $director == ''
-        or $release == '' or $crawl == '')) {
-    echo "Need to type required text!!";
-}
+
 ?>
 </body>
 </html>
