bb94c2eb536053334eb47d444aca309aaf7a9a76
Falinana <fad31@uclive.ac.nz>
2023-10-14 16:27:33 +1300

    changes to search page

--- src/js/app.js
+++ src/js/app.js
@@ -1,25 +1,91 @@
-const search=() =>{
-    const searchbox=document.getElementByID("search-item").value.toUpperCase();
-    const storeitems = document.getElementByID("product-list");
-    const product = document.querySelectorAll(".Manufacturer")||
-        document.querySelectorAll(".Person") ||
-        document.querySelectorAll(".Planet")||
-        document.querySelectorAll(".Starships") ||
-        document.querySelectorAll(".Vehicle");
-    const pname = document.getElementsByTagName("$name");
+let searchresults = {
+    data: [
+        {
+            //insert data here
+        }
+    ],
+};
 
-    for(var i=0; i < pname.length;i++){
-        let match = product[i].getElementsByTagName('h2')[0];
+for (let i of searchresults.data) {
+    //Create Card
+    let card = document.createElement("div");
+    //Card should have category and should stay hidden initially
+    card.classList.add("card", i.category, "hide");s
+    //image div
+    let imgContainer = document.createElement("div");
+    imgContainer.classList.add("image-container");
+    //img tag
+    let image = document.createElement("img");
+    image.setAttribute("src", i.image);
+    imgContainer.appendChild(image);
+    card.appendChild(imgContainer);
+    //container
+    let container = document.createElement("div");
+    container.classList.add("container");
+    //result name
+    let name = document.createElement("h5");
+    name.classList.add("searchreults");
+    name.innerText = i.searchresultsName.toUpperCase();
+    container.appendChild(name);
 
-        if(match){
-            let textvalue= match.textContent || match.innerHTML
+    card.appendChild(container);
+    document.getElementById("searchresults").appendChild(card);
+}
 
-            if(textvalue.toUpperCase().indexOf(searchbox) > -1){
-                product[i].style.display="";
-            }
-            else{
-                product[i].style.display="none";
+//parameter passed from button (Parameter same as category)
+function filtersearchresults(value) {
+    //Button class code
+    let buttons = document.querySelectorAll(".button-value");
+    buttons.forEach((button) => {
+        //check if value equals innerText
+        if (value.toUpperCase() == button.innerText.toUpperCase()) {
+            button.classList.add("active");
+        } else {
+            button.classList.remove("active");
+        }
+    });
+
+    //select all cards
+    let elements = document.querySelectorAll(".card");
+    //loop through all cards
+    elements.forEach((element) => {
+        //display all cards on 'all' button click
+        if (value == "all") {
+            element.classList.remove("hide");
+        } else {
+            //Check if element contains category class
+            if (element.classList.contains(value)) {
+                //display element based on category
+                element.classList.remove("hide");
+            } else {
+                //hide other elements
+                element.classList.add("hide");
             }
         }
-    }
+    });
 }
+
+//Search button click
+document.getElementById("search").addEventListener("click", () => {
+    //initializations
+    let searchInput = document.getElementById("search-input").value;
+    let elements = document.querySelectorAll(".searchresults");
+    let cards = document.querySelectorAll(".card");
+
+    //loop through all elements
+    elements.forEach((element, index) => {
+        //check if text includes the search value
+        if (element.innerText.includes(searchInput.toUpperCase())) {
+            //display matching card
+            cards[index].classList.remove("hide");
+        } else {
+            //hide others
+            cards[index].classList.add("hide");
+        }
+    });
+});
+
+//Initially display all search results
+window.onload = () => {
+    filtersearchresults("all");
+};
