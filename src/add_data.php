e975de088ca5c2e0ec1c816b0a298e2700e4483f
Joel Dumelow <jdu89@uclive.ac.nz>
2023-10-15 21:58:05 +1300

    Fixes to add_data

--- src/add_data.php
+++ src/add_data.php
@@ -13,7 +13,10 @@ $people = $dataArray[0];
 $planet = $dataArray[1];
 $vehicle = $dataArray[2];
 $starShip = $dataArray[3];
-
+var_dump($people);
+var_dump($planet);
+var_dump($vehicle);
+var_dump($starShip);
 
 echo count($people);
 echo count($planet);
